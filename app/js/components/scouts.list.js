09d2140c302b4d9f8486f6a7789100ab12098566
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-11 22:32:45 -0600

    Add scouts to scout list

--- app/js/components/scouts.list.js
+++ app/js/components/scouts.list.js
@@ -2,8 +2,39 @@
 
 var React = require('react');
 
+function getScouts() {
+  return {
+    scouts: [
+    {
+      name: 'Jason Hamm',
+      rank: 'Tenderfoot'
+    },
+    {
+      name: 'Tristan McGraw',
+      rank: 'Life'
+    },
+    {
+      name: 'Whitney Palmer',
+      rank: 'Star'
+    }
+  ]
+  };
+}
+
 var ScoutsList = React.createClass({
+  getInitialState: function() {
+    return getScouts();
+  },
   render: function() {
+    var scouts = this.state.scouts.map(function(scout) {
+      return (
+        <tr>
+          <td>{scout.name}</td>
+          <td>{scout.rank}</td>
+          <td><button className="btn">Full Detail</button></td>
+        </tr>
+      );
+    });
     return(
       <div className="container">
         <table className="table table-striped">
@@ -15,11 +46,7 @@ var ScoutsList = React.createClass({
             </tr>
           </thead>
             <tbody>
-              <tr>
-                <td>Hamm, Jason</td>
-                <td>Tenderfoot</td>
-                <td><button className="btn">Full Detail</button></td>
-              </tr>
+              {scouts}
             </tbody>
         </table>
       </div>
