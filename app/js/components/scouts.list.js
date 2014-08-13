189e5e5bf901c21188dbe2c341810f80fedb8577
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-12 18:59:56 -0600

    Use store to get scouts

--- app/js/components/scouts.list.js
+++ app/js/components/scouts.list.js
@@ -1,23 +1,11 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var ScoutStore = require('./../stores/scout.store.js');
 
 function getScouts() {
   return {
-    scouts: [
-    {
-      name: 'Jason Hamm',
-      rank: 'Tenderfoot'
-    },
-    {
-      name: 'Tristan McGraw',
-      rank: 'Life'
-    },
-    {
-      name: 'Whitney Palmer',
-      rank: 'Star'
-    }
-  ]
+    scouts: ScoutStore.getScouts()
   };
 }
 
@@ -25,10 +13,14 @@ var ScoutsList = React.createClass({
   getInitialState: function() {
     return getScouts();
   },
+  handleClick: function(e) {
+    console.log("clicked");
+  },
   render: function() {
+    var that = this;
     var scouts = this.state.scouts.map(function(scout) {
       return (
-        <tr>
+        <tr  onClick={that.handleClick}>
           <td>{scout.name}</td>
           <td>{scout.rank}</td>
           <td><button className="btn">Full Detail</button></td>
