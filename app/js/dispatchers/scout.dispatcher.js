74e4aeb0c1a19dc4a5524313f2e23b23cfd35115
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-16 07:30:22 -0600

    Add comment to look into

--- app/js/dispatchers/scout.dispatcher.js
+++ app/js/dispatchers/scout.dispatcher.js
@@ -5,6 +5,7 @@ var merge = require('react/lib/merge');
 var ScoutDispatcher = merge(Dispatcher.prototype, {
   handleViewAction: function(action) {
     this.dispatch({
+    	// What is the VIEW_ACTION USED FOR
       src: 'VIEW_ACTION',
       action: action
     });
