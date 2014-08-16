a5d43a6b50ca707903c89ef144a25ae400bfaba2
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-15 20:57:16 -0600

    Show scouts after REST call

--- app/js/components/scouts.list.js
+++ app/js/components/scouts.list.js
@@ -10,40 +10,48 @@ function getScouts() {
 }
 
 var ScoutsList = React.createClass({
-  getInitialState: function() {
-    return getScouts();
-  },
-  handleClick: function(e) {
-    console.log("clicked");
-  },
-  render: function() {
-    var that = this;
-    var scouts = this.state.scouts.map(function(scout) {
-      return (
-        <tr  onClick={that.handleClick}>
-          <td>{scout.name}</td>
-          <td>{scout.rank}</td>
-          <td><button className="btn">Full Detail</button></td>
-        </tr>
-      );
-    });
-    return(
-      <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rank</th>
-              <th>Full Detail</th>
-            </tr>
-          </thead>
-            <tbody>
-              {scouts}
-            </tbody>
-        </table>
-      </div>
-    );
-  }
+	getInitialState: function() {
+		return getScouts();
+	},
+	componentDidMount: function() {
+		ScoutStore.addChangeListener(this.onChange);
+	},
+	handleClick: function(e) {
+		console.log("clicked");
+	},
+	onChange: function() {
+		this.setState(getScouts());
+	},
+	render: function() {
+		var that = this;
+		var scouts = this.state.scouts.map(function(scout) {
+			return (
+				<tr  onClick={that.handleClick}>
+					<td>{scout.name}</td>
+					<td>{scout.rank}</td>
+					<td>
+						<button className="btn">Full Detail</button>
+					</td>
+				</tr>
+			);
+		});
+		return(
+			<div className="container">
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Rank</th>
+							<th>Full Detail</th>
+						</tr>
+					</thead>
+					<tbody>
+						{scouts}
+					</tbody>
+				</table>
+			</div>
+			);
+		}
 });
 
 module.exports = ScoutsList;
--- app/js/stores/scout.store.js
+++ app/js/stores/scout.store.js
@@ -1,25 +1,38 @@
 var merge = require('react/lib/merge');
 var EventEmitter = require('events').EventEmitter;
 
+var CHANGE_EVENT = "change";
 var scouts = [
-    {
-      name: 'Jason Hamm',
-      rank: 'Tenderfoot'
-    },
-    {
-      name: 'Tristan McGraw',
-      rank: 'Life'
-    },
-    {
-      name: 'Whitney Palmer',
-      rank: 'Star'
-    }
+// {
+// 	name: 'Jason Hamm',
+// 	rank: 'Tenderfoot'
+// }, {
+// 	name: 'Tristan McGraw',
+// 	rank: 'Life'
+// }, {
+// 	name: 'Whitney Palmer',
+// 	rank: 'Star'
+// }
 ];
 
 var ScoutStore = merge(EventEmitter.prototype, {
-  getScouts: function() {
-    return scouts;
-  }
+	addChangeListener: function(callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
+	getScouts: function() {
+		var that = this;
+		if(scouts.length === 0) {
+			$.ajax({
+				url: 'http://localhost:9000/scouts',
+			}).success(function(data) {
+				scouts = data.scouts;
+				that.emit(CHANGE_EVENT);
+			});
+			return scouts;
+		} else {
+			return scouts;
+		}
+	}
 });
 
 module.exports = ScoutStore;
