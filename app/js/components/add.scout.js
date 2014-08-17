7fe27b4bd0110f07df43f4ade036ef17e8a702dd
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-17 13:23:00 -0600

    Add modal to list

--- app/js/components/add.scout.js
+++ app/js/components/add.scout.js
@@ -4,14 +4,75 @@ var React = require('react');
 var ScoutsList = require('./scouts.list.js');
 var AddScout = require('./add.scout.js');
 var ScoutActions = require('./../actions/scout.actions.js');
+var OverlayMixin = require('react-bootstrap').OverlayMixin;
+var Modal = require('react-bootstrap').Modal;
+
 
 var AddScout = React.createClass({
-	handleClick: function() {
-		ScoutActions.addScout();
+	mixins: [OverlayMixin],
+	getInitialState: function() {
+		return {
+			isModalOpen: false,
+			scout: {}
+		};
+	},
+	toggleModal: function() {
+		this.state.scout = {};
+		this.setState({
+			isModalOpen: !this.state.isModalOpen
+		});
+	},
+	handleToggle: function() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen
+		});
+	},
+	makeChangeHandler: function(property) {
+		var that = this;
+		return function(e) {
+			that.state.scout[property] = e.currentTarget.value;
+			that.setState({user: that.state.user});
+		};
+	},
+	addScout: function() {
+		ScoutActions.addScout(this.state.scout);
 	},
 	render: function() {
+		return ( < button onClick = { this.toggleModal} type = "button" className = "btn btn-success" >Add Scout < /button>
+		);
+	},
+	renderOverlay: function() {
+		if (!this.state.isModalOpen) {
+			return <span></span>
+		}
 		return (
-			<button onClick={this.handleClick} type="button" className="btn btn-success">Add Scout</button>
+			< Modal title = "Add a Scout" onRequestHide = { this.handleToggle} >
+				< div className = "modal-body" >
+					<form>
+						<div className="form-group">
+							<label for="first_name">First Name</label>
+							<input className="form-control" onBlur={this.makeChangeHandler('first_name')}></input>
+						</div>
+						<div className="form-group">
+							<label for="last_name">Last Name</label>
+							<input className="form-control" onBlur={this.makeChangeHandler('last_name')}></input>
+						</div>
+						<div className="form-group">
+							<label for="rank">Rank</label>
+							<select className="form-control" onBlur={this.makeChangeHandler('rank')}>
+								<option>Boy Scout</option>
+								<option>Tenderfoot</option>
+								<option>2nd Class</option>
+								<option>First Class</option>
+							</select>
+						</div>
+					</form>
+				< /div>
+				<div className="modal-footer">
+					<button onClick={this.addScout} className="btn btn-primary">Add</button >
+					<button onClick={this.toggleModal} className="btn btn-warning">Cancel</button >
+				< /div>
+			</Modal >
 		);
 	}
 });
