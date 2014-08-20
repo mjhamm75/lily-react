2be2e4c2be998f3d786e36c50244fd03efa7dbfe
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-19 23:11:22 -0600

    Add remove scout action

--- app/js/components/remove.scout.js
+++ app/js/components/remove.scout.js
@@ -5,7 +5,7 @@ var React = require('react');
 var RemoveScout = React.createClass({
 	handleClick: function(e) {
 		e.stopPropagation();
-		console.log('clicked');
+		ScoutActions.removeScout(this.props.scoutId);
 	},
 	render: function() {
 		return <span onClick={this.handleClick} className="glyphicon glyphicon-remove-circle"></span>;
