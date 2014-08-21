7d155bcd40a989f761e226fc5632660f5318e8f8
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-20 20:53:29 -0600

    Change routes to work

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,32 @@
 var React = require('react');
 var LilyApp = require('./components/lily.app.js');
 var NavBar = require('./components/navbar.js');
+var ScoutDetail = require('./components/scout.detail.js');
+var Router = require('react-router');
+var Routes = Router.Routes;
+var Route = Router.Route;
+var DefaultRoute = Router.Default;
 
-React.renderComponent(
-  <div>
-    <NavBar />
-    <div className="container">
-      <LilyApp />
-    </div>
-  </div>,
-  document.getElementById('lily')
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<NavBar />
+				<div className="container">
+					<this.props.activeRouteHandler />
+				</div>
+			</div>
+		);
+	}
+});
+
+var routes = (
+	<Routes>
+		<Route path="/" name="app" handler={App}>
+			<Route path="/scouts" handler={LilyApp} />
+			<Route path="/scout-detail" handler={ScoutDetail} />
+		</Route>
+	</Routes>
 );
+
+React.renderComponent(routes, document.getElementById('lily'));
