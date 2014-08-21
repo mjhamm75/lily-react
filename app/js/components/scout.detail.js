f2ee9f509f09706fc2dd0bbc5bcc289cf3fbb8db
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-20 20:52:39 -0600

    Add scout-detail page

---
+++ app/js/components/scout.detail.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var ScoutsDetail = React.createClass({
+	render: function() {
+		return(<div>Jason Hamm</div>);
+	}
+});
+
+module.exports = ScoutsDetail;
