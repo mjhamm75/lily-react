619937063ff17b35a94eea4068c86df062c2f01c
Jason Hamm <jasonhamm.me@gmail.com>
2014-08-20 21:01:14 -0600

    Add html to file

--- app/js/components/scout.detail.js
+++ app/js/components/scout.detail.js
@@ -4,7 +4,55 @@ var React = require('react');
 
 var ScoutsDetail = React.createClass({
 	render: function() {
-		return(<div>Jason Hamm</div>);
+		return(
+			<div className="container">
+			  <h4>Jason Hamm</h4>
+			  <table className="table table-striped">
+			    <thead>
+			      <tr>
+			        <th>
+			          Rank
+			        </th>
+			        <th>
+			          Percent Remaining
+			        </th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <tr>
+			        <td>
+			          Boy Scout
+			        </td>
+			        <td>
+			          3 of 10 complete
+			        </td>
+			      </tr>
+			    </tbody>
+			  </table>
+			  <table className="table table-striped">
+			    <thead>
+			      <tr>
+			        <th>
+			          Merit Badge
+			        </th>
+			        <th>
+			          Percent Remaining
+			        </th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <tr>
+			        <td>
+			          Archery
+			        </td>
+			        <td>
+			          50%
+			        </td>
+			      </tr>
+			    </tbody>
+			  </table>
+			</div>
+		);
 	}
 });
 
